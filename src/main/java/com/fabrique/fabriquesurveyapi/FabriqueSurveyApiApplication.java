9426a9671b84701c33309beda016bc665888a413
Nathan <iroubtsov@gmail.com>
2022-02-14 20:04:10 +0300

    Added Spring

---
+++ src/main/java/com/fabrique/fabriquesurveyapi/FabriqueSurveyApiApplication.java
@@ -0,0 +1,13 @@
+package com.fabrique.fabriquesurveyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabriqueSurveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabriqueSurveyApiApplication.class, args);
+	}
+
+}
